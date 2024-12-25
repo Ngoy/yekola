a47b48e906cba7446bd082cad8b2c3d94f530c77
Ngoy <lucienngoy1@gmail.com>
2024-12-25 16:47:53 +0100

    la base du css du flexbox

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,114 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ScrollView,
+  Button,
+} from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
+const App = () => {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
+    <ScrollView style={styles.container}>
+      <Text style={styles.main}> Listes des produits</Text>
+
+      <View style={styles.pictureBox}>
         <Image
-          source={require('@/assets/images/docs.png')}
-          style={styles.reactLogo}
+          style={styles.picture}
+          source={{
+            uri: "https://i.pinimg.com/736x/e2/cb/29/e2cb29cd11f0ea5513b4dc0cc9842087.jpg",
+          }}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome! MR Lucien</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Premier pas</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      </View>
+
+      <View style={styles.box}>
+        <View style={styles.item1}> </View>
+        <View style={styles.item2}></View>
+        <View style={styles.item3}> </View>
+        <View style={styles.item4}></View>
+      </View>
+
+      <View style={styles.box}>
+        <View style={styles.item1}> </View>
+        <View style={styles.item2}></View>
+        <View style={styles.item3}> </View>
+        <View style={styles.item4}></View>
+      </View>
+
+      <View style={styles.box}>
+        <View style={styles.item1}> </View>
+        <View style={styles.item2}></View>
+        <View style={styles.item3}> </View>
+        <View style={styles.item4}></View>
+      </View>
+    </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    backgroundColor: "#C41C57",
+    padding: 10,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  box: {
+    backgroundColor: "white",
+    height: 400,
+    width: "auto",
+    margin: 10,
+    // padding: 20,
+    // marginRight : 20,
+
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  item1: {
+    backgroundColor: "blue",
+    height: 100,
+    width: 50,
+  },
+
+  item2: {
+    backgroundColor: "red",
+    height: 100,
+    width: 50,
+  },
+  item3: {
+    backgroundColor: "yellow",
+    height: 100,
+    width: 50,
+  },
+  item4: {
+    backgroundColor: "green",
+    height: 100,
+    width: 50,
+  },
+  main: {
+    backgroundColor: "white",
+    fontWeight: 900,
+    fontSize: 30,
+    textAlign: "center",
+    color: "blue",
+    textTransform: "capitalize",
+  },
+  pictureBox: {
+    backgroundColor: "blue",
+    width: "auto",
+    height: 300,
+    borderRadius: 20,
+    overflow: "hidden",
+  },
+
+  picture: {
+    // width: 150,
+    // height: 150,
+    width: "auto",
+    height: "100%",
+    resizeMode: "stretch",
   },
 });
+
+export default App;
